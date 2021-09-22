5852170882f38e4a3825ee516ebd5d223c8a7534
shahnaz seharti <shahnazsehati@gmail.com>
2021-09-22 11:13:06 +0330

    first commit

--- leaflet-example.js
+++ leaflet-example.js
@@ -7,13 +7,235 @@
 
  The source code below is the example from the leaflet start page.
  */
+// 18104, -51192
+var map = L.map("map").setView([0, 0], 1);
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var md5 = CryptoJS.MD5(
+  "feb243e6-a047-4a00-a446-12cef6b3df6a/fa1d931e-19fc-11ec-b5e5-8a1a0b6c6c3d/4/3/6.png"
+).toString();
+console.log(md5);
 
-L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.TileLayer.pdf = L.TileLayer.extend({
+  getTileUrl: function (coords) {
+    // [top,left]=this.option.location
+    const tileUrl = L.TileLayer.prototype.getTileUrl.call(this, coords);
+    const completeTileUrl = `https://sp-projects-files-v1-dev.s3-ap-southeast-2.amazonaws.com/${CryptoJS.MD5(
+      tileUrl
+    )}/${tileUrl}`;
+    console.log(completeTileUrl);
+    return completeTileUrl;
+  },
+  coordsIsValid(coords, bounds, zoomLevelsProperties) {
+    var tileSize = 256;
+    const maxX = Math.abs(bounds.maxX - bounds.minX);
+    const maxY = Math.abs(bounds.maxY - bounds.minY);
+    const zoomLevelProperties = zoomLevelsProperties.filter(
+      (value) => value.order === coords.z
+    );
+    if (!!zoomLevelProperties?.[0]?.unitPerPixel) {
+      const unitPerPixel = zoomLevelProperties[0].unitPerPixel;
+      const tilesMaxColumns = Math.floor(maxX / tileSize / unitPerPixel);
+      const tilesMaxRows = Math.floor(maxY / tileSize / unitPerPixel);
+      return (
+        coords.z >= 0 &&
+        coords.x >= 0 &&
+        coords.y >= 0 &&
+        coords.x <= tilesMaxColumns &&
+        coords.y <= tilesMaxRows
+      );
+    }
 
-L.marker([51.5, -0.09]).addTo(map)
-		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-		.openPopup();
+    return coords.z >= 0 && coords.x >= 0 && coords.y >= 0; // && (isCoordsValidForFileMetadata(coords, fileMetadata));
+  },
+  getMaxColumnRow(bounds, minZoomLevelProperty) {
+    var tileSize = 256;
+    const maxX = Math.abs(bounds.maxX - bounds.minX);
+    const maxY = Math.abs(bounds.maxY - bounds.minY);
+    const unitPerPixel = minZoomLevelProperty.unitPerPixel;
+    const tilesMaxColumns = maxX / tileSize / unitPerPixel;
+    const tilesMaxRows = maxY / tileSize / unitPerPixel;
+    return {
+      tilesMaxColumns,
+
+      tilesMaxRows,
+    };
+  },
+
+  createTile: function (coords) {
+    var tile = document.createElement("img");
+    tile.innerHTML = [coords.x, coords.y, coords.z].join(", ");
+    tile.style.outline = "1px solid red";
+    coords.x = Math.ceil(
+      coords.x -
+        this.getMaxColumnRow(
+          this.options.myBounds,
+          this.options.zoomLevelsProperties[coords.z]
+        ).tilesMaxColumns *
+          this.options.left
+    );
+    coords.y = Math.ceil(
+      coords.y -
+        this.getMaxColumnRow(
+          this.options.myBounds,
+          this.options.zoomLevelsProperties[coords.z]
+        ).tilesMaxRows *
+          this.options.top
+    );
+    if (
+      this.coordsIsValid(
+        coords,
+        this.options.myBounds,
+        this.options.zoomLevelsProperties
+      )
+    ) {
+      const tileUrl = this.getTileUrl(coords);
+      tile.setAttribute("src", tileUrl);
+
+      // fetch(tileUrl)
+      //     .then(response => console.log('hereeeeeeeeeeeee', response)
+
+      //     )
+      //     .catch(error => console.log(error))
+      //     .finally(() => {
+      //          if(response.ok){
+      //     //  const blobUrl = URL.createObjectURL(response);
+      //             //   invokeUrlService.registerBlobUrl(blobUrl);
+      //             //   setTileUrl(self, done, tile, blobUrl);
+      //             tile.setAttribute('src', response.url);
+      //             done(null, tile);
+      //      }
+      // });
+
+      // var xmlHttp = new XMLHttpRequest();
+      // xmlHttp.onreadystatechange = function() {
+      //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      //         // callback(xmlHttp.responseText);
+      //         tile.setAttribute('src', xmlHttp.responseText);
+
+      //             done(null, tile)
+      // }
+      // xmlHttp.open("GET", tileUrl, true); // true for asynchronous
+      // xmlHttp.send(null);
+      //  tile.setAttribute('src', tileUrl);
+    }
+    return tile;
+  },
+
+  getAttribution: function () {
+    return "<a href='https://placekitten.com/attribution.html'>PlaceKitten</a>";
+  },
+});
+
+file1 = new L.TileLayer.pdf(
+  "feb243e6-a047-4a00-a446-12cef6b3df6a/fa1d931e-19fc-11ec-b5e5-8a1a0b6c6c3d/{z}/{x}/{y}.png",
+  {
+    // zoomOffset: -1,
+    noWrap: true,
+    top: 0,
+    left: 0,
+    myBounds: {
+      maxX: 36208,
+      maxY: 0,
+      minX: 0,
+      minY: -102384,
+    },
+    zoomLevelsProperties: [
+      { unitPerPixel: 256, order: 1 },
+      { unitPerPixel: 128, order: 2 },
+      { unitPerPixel: 64, order: 3 },
+      { unitPerPixel: 32, order: 4 },
+      { unitPerPixel: 16, order: 5 },
+      { unitPerPixel: 8, order: 6 },
+      { unitPerPixel: 4, order: 7 },
+      { unitPerPixel: 2, order: 8 },
+      { unitPerPixel: 1, order: 9 },
+    ],
+  }
+);
+
+map.addLayer(file1);
+file2 = new L.TileLayer.pdf(
+  "feb243e6-a047-4a00-a446-12cef6b3df6a/fa1d7f85-19fc-11ec-b5e5-8a1a0b6c6c3d/{z}/{x}/{y}.png",
+  {
+    // zoomOffset: -1,
+    noWrap: true,
+    top: 0,
+    left: 1,
+    myBounds: {
+      maxX: 36192,
+      maxY: 0,
+      minX: 0,
+      minY: -102384,
+    },
+    zoomLevelsProperties: [
+      { unitPerPixel: 256, order: 1 },
+      { unitPerPixel: 128, order: 2 },
+      { unitPerPixel: 64, order: 3 },
+      { unitPerPixel: 32, order: 4 },
+      { unitPerPixel: 16, order: 5 },
+      { unitPerPixel: 8, order: 6 },
+      { unitPerPixel: 4, order: 7 },
+      { unitPerPixel: 2, order: 8 },
+      { unitPerPixel: 1, order: 9 },
+    ],
+  }
+);
+
+map.addLayer(file2);
+
+file3 = new L.TileLayer.pdf(
+  "feb243e6-a047-4a00-a446-12cef6b3df6a/fa1d964f-19fc-11ec-b5e5-8a1a0b6c6c3d/{z}/{x}/{y}.png",
+  {
+    // zoomOffset: -1,
+    top: 0,
+    noWrap: true,
+    left: 2,
+    myBounds: {
+      maxX: 36208,
+      maxY: 0,
+      minX: 0,
+      minY: -102384,
+    },
+    zoomLevelsProperties: [
+      { unitPerPixel: 256, order: 1 },
+      { unitPerPixel: 128, order: 2 },
+      { unitPerPixel: 64, order: 3 },
+      { unitPerPixel: 32, order: 4 },
+      { unitPerPixel: 16, order: 5 },
+      { unitPerPixel: 8, order: 6 },
+      { unitPerPixel: 4, order: 7 },
+      { unitPerPixel: 2, order: 8 },
+      { unitPerPixel: 1, order: 9 },
+    ],
+  }
+);
+
+map.addLayer(file3);
+file4 = new L.TileLayer.pdf(
+  "feb243e6-a047-4a00-a446-12cef6b3df6a/fa1d8f57-19fc-11ec-b5e5-8a1a0b6c6c3d/{z}/{x}/{y}.png",
+  {
+    // zoomOffset: -1,
+    noWrap: true,
+    top: 0,
+    left: 3,
+    myBounds: {
+      maxX: 36192,
+      maxY: 0,
+      minX: 0,
+      minY: -102384,
+    },
+    zoomLevelsProperties: [
+      { unitPerPixel: 256, order: 1 },
+      { unitPerPixel: 128, order: 2 },
+      { unitPerPixel: 64, order: 3 },
+      { unitPerPixel: 32, order: 4 },
+      { unitPerPixel: 16, order: 5 },
+      { unitPerPixel: 8, order: 6 },
+      { unitPerPixel: 4, order: 7 },
+      { unitPerPixel: 2, order: 8 },
+      { unitPerPixel: 1, order: 9 },
+    ],
+  }
+);
+
+map.addLayer(file4);
