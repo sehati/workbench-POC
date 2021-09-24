e4e333d42bfe55e4920fc85819f9886420290c69
shahnaz seharti <shahnazsehati@gmail.com>
2021-09-24 19:34:30 +0330

    transform tiles to remove gaps

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # workbench-POC
-
-I assumed that size of all pdfs are equal. so there is some gap tiles.
--- leaflet-example.js
+++ leaflet-example.js
@@ -1,3 +1,81 @@
+const workbenchPDFs = {
+  file11: {
+    myBounds: {
+      maxX: 36208,
+      maxY: 0,
+      minX: 0,
+      minY: -102384,
+    },
+    zoomLevelsProperties: [
+      { unitPerPixel: 256, order: 1 },
+      { unitPerPixel: 128, order: 2 },
+      { unitPerPixel: 64, order: 3 },
+      { unitPerPixel: 32, order: 4 },
+      { unitPerPixel: 16, order: 5 },
+      { unitPerPixel: 8, order: 6 },
+      { unitPerPixel: 4, order: 7 },
+      { unitPerPixel: 2, order: 8 },
+      { unitPerPixel: 1, order: 9 },
+    ],
+  },
+  file12: {
+    myBounds: {
+      maxX: 36192,
+      maxY: 0,
+      minX: 0,
+      minY: -102384,
+    },
+    zoomLevelsProperties: [
+      { unitPerPixel: 256, order: 1 },
+      { unitPerPixel: 128, order: 2 },
+      { unitPerPixel: 64, order: 3 },
+      { unitPerPixel: 32, order: 4 },
+      { unitPerPixel: 16, order: 5 },
+      { unitPerPixel: 8, order: 6 },
+      { unitPerPixel: 4, order: 7 },
+      { unitPerPixel: 2, order: 8 },
+      { unitPerPixel: 1, order: 9 },
+    ],
+  },
+  file13: {
+    myBounds: {
+      maxX: 36208,
+      maxY: 0,
+      minX: 0,
+      minY: -102384,
+    },
+    zoomLevelsProperties: [
+      { unitPerPixel: 256, order: 1 },
+      { unitPerPixel: 128, order: 2 },
+      { unitPerPixel: 64, order: 3 },
+      { unitPerPixel: 32, order: 4 },
+      { unitPerPixel: 16, order: 5 },
+      { unitPerPixel: 8, order: 6 },
+      { unitPerPixel: 4, order: 7 },
+      { unitPerPixel: 2, order: 8 },
+      { unitPerPixel: 1, order: 9 },
+    ],
+  },
+  file14: {
+    myBounds: {
+      maxX: 36192,
+      maxY: 0,
+      minX: 0,
+      minY: -102384,
+    },
+    zoomLevelsProperties: [
+      { unitPerPixel: 256, order: 1 },
+      { unitPerPixel: 128, order: 2 },
+      { unitPerPixel: 64, order: 3 },
+      { unitPerPixel: 32, order: 4 },
+      { unitPerPixel: 16, order: 5 },
+      { unitPerPixel: 8, order: 6 },
+      { unitPerPixel: 4, order: 7 },
+      { unitPerPixel: 2, order: 8 },
+      { unitPerPixel: 1, order: 9 },
+    ],
+  },
+};
 var map = L.map("map").setView([0, 0], 1);
 
 L.TileLayer.pdf = L.TileLayer.extend({
@@ -40,7 +118,6 @@ L.TileLayer.pdf = L.TileLayer.extend({
     const tilesMaxRows = maxY / tileSize / unitPerPixel;
     return {
       tilesMaxColumns,
-
       tilesMaxRows,
     };
   },
@@ -48,15 +125,36 @@ L.TileLayer.pdf = L.TileLayer.extend({
   createTile: function (coords) {
     var tile = document.createElement("img");
     tile.innerHTML = [coords.x, coords.y, coords.z].join(", ");
-    tile.style.outline = "1px solid red";
-    coords.x = Math.ceil(
-      coords.x -
-        this.getMaxColumnRow(
-          this.options.myBounds,
-          this.options.zoomLevelsProperties[coords.z]
-        ).tilesMaxColumns *
-          this.options.left
-    );
+    // tile.style.outline = "1px solid red";
+    let leftOffset = 0;
+    let leftPixels = 0;
+    for (let x = 1; x <= this.options.left; x++) {
+      maxColumnRow = this.getMaxColumnRow(
+        workbenchPDFs[`file${this.options.top + 1}${x}`].myBounds,
+        workbenchPDFs[
+          `file${this.options.top + 1}${x}`
+        ].zoomLevelsProperties.filter((value) => value.order === coords.z)[0]
+      );
+      leftOffset = maxColumnRow.tilesMaxColumns + leftOffset;
+    }
+    // console.log(this.options.left, leftOffset);
+    coords.x = coords.x - parseInt(leftOffset);
+    const translateX = (leftOffset - parseInt(leftOffset)) * 256;
+    console.log(translateX, this.options.left);
+    const regex = /(\d+)px/;
+    setTimeout(function () {
+      // console.log(tile.style.transform.replace(regex, matcher));
+      tile.style.transform = tile.style.transform.replace(
+        regex,
+        function (match, p1, p2, p3) {
+          return `${parseInt(p1) + translateX + ""}px`;
+        }
+      );
+    }, 100);
+    // tile.style.transform = `translateX(${
+    //   (leftOffset - parseInt(leftOffset)) *
+    //   this.options.zoomLevelsProperties[coords.z]
+    // }px)`;
     coords.y = Math.ceil(
       coords.y -
         this.getMaxColumnRow(
